e0d9920b16664f2728742d3c2d0d194966c4ecf2
nekobuff69 <sagathecanontese@gmail.com>
2025-12-21 23:27:26 +0800

    feat(ui): update default settings values
    Update character, mood, environment, and theme default values to new options for enhanced user experience.

--- src/data/defaultSettings.js
+++ src/data/defaultSettings.js
@@ -3,27 +3,24 @@ export const defaultSettings = [
 		index: 0,
 		id: "character",
 		description: "Choose your tale's heroes:",
-		value: ["海绵宝宝 比卡超"],
+		value: ["海绵宝宝 孙悟空"],
 	},
 	{
 		index: 1,
 		id: "mood",
 		description: "Set the adventure's heartbeat:",
-		value: ["欢乐 古灵精怪"],
+		value: ["奇幻"],
 	},
 	{
 		index: 2,
 		id: "environment",
 		description: "Discover a realm of wonder:",
-		value: ["旁晚沙漠", "龙卷风来袭"],
+		value: ["伦敦大笨钟", "暴风雪"],
 	},
 	{
 		index: 3,
 		id: "theme",
 		description: "Uncover the story's secret spark:",
-		value: [
-			"勇气 智慧",
-			// "Alien Encounter"
-		],
+		value: ["友谊", "勇气"],
 	},
 ];
